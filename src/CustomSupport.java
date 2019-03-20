9d3134267d451339067520b1bd1d210bc157e3ae
DevBorisElkin <ea.foreverrr@gmail.com>
2019-03-20 11:32:56 +0300

    Много изменений и улучшений.

--- src/CustomSupport.java
+++ src/CustomSupport.java
@@ -25,33 +25,40 @@ public class CustomSupport {
     }
 
     public static void searchFor(String column, String data){
+        boolean oneTimeColumn=true;
         List<DataCell> header = Main.dataList.get(0);
         int counter, i;
         counter=0;
         i=0;
         for(DataCell a: header){
-            if(a.data.toString().equals(column)) {System.out.println("Колонка с именем \""+column+"\" была найдена под индексом "+(i)+" - считая от 0"); counter=i;}
+            if(a.data.toString().equals(column)&&oneTimeColumn) {
+                System.out.println("Колонка с именем \""+column+"\" была найдена под индексом "+(i)+" - считая от 0");
+                checkRequestForAccuracy(a.headerType, data);
+                counter=i;  oneTimeColumn=false;}
             i++;
         }
         i=0;
         for(List<DataCell> list: Main.dataList){
             DataCell b=list.get(counter);
             if(b.data.toString().equals(data)) {System.out.println("Данные с \""+data+"\" были найдены под индексом "+i);
-                saveToCSV(Main.dataList.get(i),"cake.csv");
+                saveToCSV(Main.dataList.get(i));
+                if(!Main.multimpleResults){
+                    break;
+                }
             }
             i++;
         }
     }
 
-    public static void saveToCSV(List<DataCell> list, String filepath){
+    public static void saveToCSV(List<DataCell> list){
         if(Main.oneTimeRefreshingFile){
-            try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filepath), //Здесь просто создаю или обновляю файл если уже создан
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(Main.OutputName), //Здесь просто создаю или обновляю файл если уже создан
                     Main.ENCODING))) { writer.write(""); } catch (Exception e) { e.printStackTrace(); }
             Main.oneTimeRefreshingFile=false;
         }
 
         try{
-            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(filepath, true)));
+            PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(Main.OutputName, true)));
             StringBuilder sb = new StringBuilder("");
             for(DataCell cell:list){ sb.append(cell.data.toString()+Main.DELIMITER); }
             sb.deleteCharAt(sb.lastIndexOf(";"));
@@ -61,6 +68,38 @@ public class CustomSupport {
             printWriter.close();
         }catch(Exception e){
 
+        }
+    }
+
+    public static void checkRequestForAccuracy(Main.DataType columnDatatype,String data){
+        //STRING, DATE, INTEGER, FLOAT, HEADING, WRONG_TYPE, NONE
+        boolean correct = true;
+        boolean b_string=false;
+        Object a = new Object();
+        switch (columnDatatype){
+            case STRING:
+                //Здесь без проверок так как тип переменной изначально String.
+                b_string=true;
+                break;
+            case FLOAT:
+                try{a = Float.parseFloat(data);
+                } catch (NumberFormatException e){ correct=false; }
+                break;
+            case INTEGER:
+                try{a = Integer.parseInt(data);
+                } catch (NumberFormatException e){ correct=false; }
+                break;
+            case DATE:
+                if(SimpleDate.checkIfDateIsWrong(data)){ correct=false;
+                }else{ a=new SimpleDate(data);}
+                break;
+        }
+        if(!correct&!b_string) {
+            System.out.println("Ошибка в поисковом запросе. Поиск в колонке с типом "+columnDatatype+" значение имеет другой тип");
+        }else if(b_string){
+            System.out.println("Ошибка в поисковом запросе. Поиск в колонке с типом "+columnDatatype+", значение имеет тип String");
+        }else{
+            System.out.println("Неизвестная ошибка в типах");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,20 @@ import java.util.Scanner;
 public class Main {
     public static final String DELIMITER = ";";
     public static final int SPACE_GAP=30;
-    public static String ENCODING = "utf-8";
 
-    public static boolean oneTimeRefreshingFile=true;
-    public static int WrongDataErrorCounter = 0;
+    //Стандарные значения, меняются на введённые
+    public static String InputName = "input.csv";       //
+    public static String OutputName = "output.csv";     // Значения заданы, но мы их меняем вводя свои знач. из консоли.
+    public static String ENCODING = "utf-8";            //
+    public static String ColumnToSearch;
+    public static String DataToSearch;
+
+    public static boolean simplifiedSearchRequest = true;   // Если True, упрощённых поисковый запрос. Если false - поиск.запрос как в примере из задания.
+    public static boolean multimpleResults = false;         // Если True, выведет ВСЕ значения в нужной колонке по поисковому запросу, если False - только первое.
+    public static boolean printListToConsole = true;
+
+    public static boolean oneTimeRefreshingFile=true;   // Используется для создания или пересоздания файла вывода
+    public static int WrongDataErrorCounter = 0;        // Счётчик для ошибок в данных
 
     public static List<List<DataCell>> dataList = new ArrayList<>();
     public enum DataType { STRING, DATE, INTEGER, FLOAT, HEADING, WRONG_TYPE, NONE}
@@ -25,110 +35,131 @@ public class Main {
     }
 
     private static void action(String[]args) throws FileNotFoundException {
-        List<DataCell> sublist=new ArrayList<>();
+
 
         if(args.length>0){
-            String pathname = args[1];
-            Scanner scanner = new Scanner(new File(pathname));
-            //Scanner scanner = new Scanner(new File("table2.csv"));  //Это на случай если хотим быстро запустить без аргументов. Также не забываем закомментировать лишнее.
-            scanner.useDelimiter(DELIMITER);
-
-            String dt=scanner.nextLine();
-
-            String[] tkns = dt.split(DELIMITER);
-            for(String a:tkns){
-                String[]tmp=a.split(" ");
-                DataCell cell = new DataCell(tmp[0], DataType.HEADING);
-                cell.isTypeCorrect();   //Проверка на правильность данных
-                sublist.add(cell);
-                switch (tmp[1]){
-                    case "String":
-                        dataTypeList.add(DataType.STRING);
-                        cell.headerType=DataType.STRING;
-                        break;
-                    case "Date":
-                        dataTypeList.add(DataType.DATE);
-                        cell.headerType=DataType.DATE;
-                        break;
-                    case "Integer":
-                        dataTypeList.add(DataType.INTEGER);
-                        cell.headerType=DataType.INTEGER;
-                        break;
-                    case "Float":
-                        dataTypeList.add(DataType.FLOAT);
-                        cell.headerType=DataType.FLOAT;
-                        break;
-                    default:
-                        System.out.println("Ошибка! Неизвестный тип данных. Это может повлечь за собой дальнейшие ошибки.");
-                        dataTypeList.add(DataType.WRONG_TYPE);
-                        cell.headerType=DataType.WRONG_TYPE;
-                        break;
-                }
+            //Здесь проверяем, усложнённый ли поисковый запрос, или упрощённый. В урощённом убраны лишние ключевые слова.
+            if(simplifiedSearchRequest){
+                InputName=args[0];
+                OutputName=args[1];
+                ENCODING=args[2];
+                ColumnToSearch=args[3];
+                DataToSearch= args[4];
+            }else{
+                InputName = args[1];
+                OutputName = args[3];
+                ENCODING = args[5];
+                ColumnToSearch = args[7];
+                DataToSearch = args[9];
             }
-            dataList.add(sublist);
 
-            while(scanner.hasNext()){
-                String data=scanner.nextLine();
-                String[]tokens=data.split(DELIMITER);
-                sublist=new ArrayList<>();
-                for(int i=0;i<tokens.length;i++){
-                    DataType dataType=dataTypeList.get(i);
-                    Object a = new Object();
-                    switch (dataType){
-                        case STRING:
-                        a=tokens[i]; //Здесь без проверок так как тип переменной изначально String.
-                            //a.matches(".*\\d.*");  Интересная строчка кода. Вернёт true если в String'е будут цифры.
-                            //Добавлять проверку на цифры не стал, так как для типа String нормально хранить числа внутри.
-                            break;
-                        case FLOAT:
-                            try{a = Float.parseFloat(tokens[i]);
-                            } catch (NumberFormatException e){
-                                //e.printStackTrace();
-                                dataType= DataType.WRONG_TYPE;
-                                WrongDataErrorCounter++;
-                                a=String.valueOf(tokens[i]);
-                            }
-                            break;
-                        case INTEGER:
-                            try{a = Integer.parseInt(tokens[i]);
-                            } catch (NumberFormatException e){
-                                dataType= DataType.WRONG_TYPE;
-                                WrongDataErrorCounter++;
-                                a=String.valueOf(tokens[i]);
-                            }
-                            break;
-                        case DATE:
-                            if(SimpleDate.checkIfDateIsWrong(tokens[i])){
-                                dataType= DataType.WRONG_TYPE;
-                                WrongDataErrorCounter++;
-                                a=String.valueOf(tokens[i]);
-                            }else{
-                                a=new SimpleDate(tokens[i]);
-                            }
-                            break;
-                        case WRONG_TYPE:
-                            dataType= DataType.WRONG_TYPE;
-                            WrongDataErrorCounter++;
-                            a=String.valueOf(tokens[i]);
-                            break;
-                    }
-                    DataCell cell = new DataCell(a,dataType);
-                    cell.isTypeCorrect();
-                    sublist.add(cell);
-                }
-                dataList.add(sublist);
-            }
-            scanner.close();
+            convertDataToCells();   // Производим запись в ячейки данных  а также проверку если это возможно
 
-            CustomSupport.printList(dataList);
-            CustomSupport.searchFor("country","USA");
-            System.out.println("Ошибок в типах данных: "+ WrongDataErrorCounter);
+            if(printListToConsole)CustomSupport.printList(dataList);                                      // Выводим записанный список в консоль
+            CustomSupport.searchFor(ColumnToSearch,DataToSearch);                   // Производим поиск значения(ний) и запись в текстовый файл
+            System.out.println("Ошибок в типах данных: "+ WrongDataErrorCounter);   // Просто сообщение об ошибках в типах данных
 
         }else{
-            System.out.println("No arguments");
+            System.out.println("No arguments, try again");
         }
     }
 
+    private static void convertDataToCells() throws FileNotFoundException {
+        List<DataCell> sublist=new ArrayList<>();
+        Scanner scanner = new Scanner(new File(InputName));
+        //Scanner scanner = new Scanner(new File("table2.csv"));  //Это на случай если хотим быстро запустить без аргументов. Также не забываем закомментировать лишнее.
+        scanner.useDelimiter(DELIMITER);
+
+        String dt=scanner.nextLine();
+
+        String[] tkns = dt.split(DELIMITER);
+        for(String a:tkns){
+            String[]tmp=a.split(" ");
+            DataCell cell = new DataCell(tmp[0], DataType.HEADING);
+            cell.isTypeCorrect();   //Проверка на правильность данных
+            sublist.add(cell);
+            switch (tmp[1]){
+                case "String":
+                    dataTypeList.add(DataType.STRING);
+                    cell.headerType= DataType.STRING;
+                    break;
+                case "Date":
+                    dataTypeList.add(DataType.DATE);
+                    cell.headerType= DataType.DATE;
+                    break;
+                case "Integer":
+                    dataTypeList.add(DataType.INTEGER);
+                    cell.headerType= DataType.INTEGER;
+                    break;
+                case "Float":
+                    dataTypeList.add(DataType.FLOAT);
+                    cell.headerType= DataType.FLOAT;
+                    break;
+                default:
+                    System.out.println("Ошибка! Неизвестный тип данных. Это может повлечь за собой дальнейшие ошибки.");
+                    dataTypeList.add(DataType.WRONG_TYPE);
+                    cell.headerType= DataType.WRONG_TYPE;
+                    break;
+            }
+        }
+        dataList.add(sublist);
+
+        while(scanner.hasNext()){
+            String data=scanner.nextLine();
+            String[]tokens=data.split(DELIMITER);
+            sublist=new ArrayList<>();
+            for(int i=0;i<tokens.length;i++){
+                DataType dataType=dataTypeList.get(i);
+                Object a = new Object();
+                switch (dataType){
+                    case STRING:
+                    a=tokens[i]; //Здесь без проверок так как тип переменной изначально String.
+                        //a.matches(".*\\d.*");  Интересная строчка кода. Вернёт true если в String'е будут цифры.
+                        //Добавлять проверку на цифры не стал, так как для типа String нормально хранить числа внутри.
+                        break;
+                    case FLOAT:
+                        try{a = Float.parseFloat(tokens[i]);
+                        } catch (NumberFormatException e){
+                            //e.printStackTrace();
+                            dataType= DataType.WRONG_TYPE;
+                            WrongDataErrorCounter++;
+                            a=String.valueOf(tokens[i]);
+                        }
+                        break;
+                    case INTEGER:
+                        try{a = Integer.parseInt(tokens[i]);
+                        } catch (NumberFormatException e){
+                            dataType= DataType.WRONG_TYPE;
+                            WrongDataErrorCounter++;
+                            a=String.valueOf(tokens[i]);
+                        }
+                        break;
+                    case DATE:
+                        if(SimpleDate.checkIfDateIsWrong(tokens[i])){
+                            dataType= DataType.WRONG_TYPE;
+                            WrongDataErrorCounter++;
+                            a=String.valueOf(tokens[i]);
+                        }else{
+                            a=new SimpleDate(tokens[i]);
+                        }
+                        break;
+                    case WRONG_TYPE:
+                        dataType= DataType.WRONG_TYPE;
+                        WrongDataErrorCounter++;
+                        a=String.valueOf(tokens[i]);
+                        break;
+                    default:
+                        System.out.println("FATAL ERROR");
+                        break;
+                }
+                DataCell cell = new DataCell(a,dataType);
+                cell.isTypeCorrect();
+                sublist.add(cell);
+            }
+            dataList.add(sublist);
+        }
+        scanner.close();
+    }
 
 
 }
